56c8db40041fb9ec9bcda11dd4e81e0d04bf1988
katarzynawilczak <kasia.wilczak@yahoo.com>
2020-03-16 20:57:41 +0100

    part 6 done

--- core/src/com/hechsmanwilczak/ecorun/Screens/PlayScreen.java
+++ core/src/com/hechsmanwilczak/ecorun/Screens/PlayScreen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.hechsmanwilczak.ecorun.EcoRun;
@@ -16,22 +20,48 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gamecam;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
 
     public PlayScreen(EcoRun game) {
         this.game = game;
         gamecam = new OrthographicCamera();
         gameport = new FitViewport(EcoRun.V_WIDTH, EcoRun.V_HEIGHT, gamecam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("ecorun-lvl1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gamecam.position.set(gameport.getWorldWidth() / 2, gameport.getWorldHeight() / 2, 0);
+
+
     }
     @Override
     public void show() {
 
     }
 
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt;
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); //oo to opengl fun
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
