ad1add1faed427e1acb52f068dc950663f86bfd3
Julia Takagi <takagi002@gannon.edu>
2022-11-03 19:06:39 -0400

    updated file structures

--- test/com/hechsmanwilczak/ecorun/test/RegressionTests.java
+++ test/com/hechsmanwilczak/ecorun/test/RegressionTests.java
@@ -1,10 +1,15 @@
+package com.hechsmanwilczak.ecorun.test;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+
+
+
 @RunWith(JUnit4.class)
 public class RegressionTests {
-
 
 // in game functions
 @Test
