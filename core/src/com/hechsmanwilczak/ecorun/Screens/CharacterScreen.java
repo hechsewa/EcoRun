6a09ec99790170306e5d51a02d88dd09dae7fcaa
creely001 <creely001@gannon.edu>
2022-11-29 00:21:05 -0500

    added character buttons to Characterscreen

--- core/src/com/hechsmanwilczak/ecorun/Screens/CharacterScreen.java
+++ core/src/com/hechsmanwilczak/ecorun/Screens/CharacterScreen.java
@@ -15,6 +15,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
@@ -42,6 +43,11 @@ public class CharacterScreen implements Screen {
         fontButton = new BitmapFont(Gdx.files.internal("font.fnt"));
         fontInfo = new BitmapFont();
         fontInfo.getData().setScale(0.7f);
+        float btnWidth = EcoRun.V_WIDTH/4f; //100
+        float btnHeight = EcoRun.V_HEIGHT/5f; //41
+        float btnPosX = EcoRun.V_WIDTH/8f; //50
+        float btnPosY = (3f*EcoRun.V_HEIGHT)/7f; //90
+        float btnPosMov = EcoRun.V_HEIGHT/6f; //35
 
         Label.LabelStyle fontBtn = new Label.LabelStyle(fontButton, Color.WHITE);
         Label.LabelStyle fontIn = new Label.LabelStyle(fontInfo, Color.WHITE);
@@ -53,9 +59,49 @@ public class CharacterScreen implements Screen {
         table.setFillParent(true);
         table.setBackground(new TextureRegionDrawable(texRegBg));
 
-        Label infoLabel = new Label("Select a Character: ", fontBtn);
-        Label infoTextLabel = new Label("", fontIn);
-        infoTextLabel.setAlignment(Align.center);
+        //TextBtn style
+        TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle();
+        textButtonStyle.font = new BitmapFont(Gdx.files.internal("font.fnt"));
+
+        //Earth Character
+        TextButton earthBtn =new TextButton("Earth",textButtonStyle);
+        earthBtn.setText("Earth");
+        earthBtn.setHeight(btnHeight);
+        earthBtn.setWidth(btnWidth);
+        earthBtn.setPosition(btnPosX,btnPosY);
+        earthBtn.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                goToGameScreen();
+            }
+        });
+
+        //Saturn Character
+        TextButton saturnBtn =new TextButton("Saturn",textButtonStyle);
+        saturnBtn.setText("Saturn");
+        saturnBtn.setHeight(btnHeight);
+        saturnBtn.setWidth(btnWidth);
+        saturnBtn.setPosition(btnPosX,btnPosY);
+        saturnBtn.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                goToGameScreen();
+            }
+        });
+
+        //GraySpot Character
+        TextButton graySpotBtn =new TextButton("GraySpot",textButtonStyle);
+        graySpotBtn.setText("GraySpot");
+        graySpotBtn.setHeight(btnHeight);
+        graySpotBtn.setWidth(btnWidth);
+        graySpotBtn.setPosition(btnPosX,btnPosY);
+        graySpotBtn.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                goToGameScreen();
+            }
+        });
+
 
         Image backToMenuButton = new Image(new Texture("back.png"));
         backToMenuButton.setSize(125, 125);
@@ -66,9 +112,11 @@ public class CharacterScreen implements Screen {
             }
         });
 
-        table.add(infoLabel).expandX();
+        table.add(earthBtn).expandX();
         table.row();
-        table.add(infoTextLabel).expandX().padTop(15f);
+        table.add(saturnBtn).expandX().padTop(15f);
+        table.row();
+        table.add(graySpotBtn).expandX().padTop(15f);
         table.row();
         table.add(backToMenuButton).expandX().padTop(20f);
 
@@ -77,6 +125,10 @@ public class CharacterScreen implements Screen {
     }
     public void goToMenu(){
         game.setScreen(new MenuScreen((EcoRun) game));
+        dispose();
+    }
+    public void goToGameScreen(){
+        game.setScreen(new LevelsScreen((EcoRun) game));
         dispose();
     }
     @Override
